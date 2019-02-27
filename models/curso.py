e1f7fe18664c075770ec7fe9d5ec911f7fd5836a
jpgonzalezo <juan.gonzalez.o@usach.com>
2019-02-26 23:44:05 -0300

    ADD: Modelo curso

--- models/curso.py
+++ models/curso.py
@@ -1,16 +1,9 @@
 from db import db
 from datetime import datetime
-from models.pregunta import Pregunta
-from models.asignatura import Asignatura
-#from models.institucion import Institucion
 from models.alumno import Alumno
  
 import mongoengine_goodjson as gj
 class Curso(gj.Document):
     nombre = db.StringField(verbose_name="Nombre curso", max_length=200)
-    fecha_creacion = db.DateTimeField(default=datetime.now)
-    preguntas = db.ListField(db.EmbeddedDocumentField(Pregunta))
-    asignatura = db.ReferenceField(Asignatura)
-    #institucion = db.ReferenceField(Institucion)
     alumnos = db.ListField(db.ReferenceField(Alumno))
     meta = {'strict': False}
