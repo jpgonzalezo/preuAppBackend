dfd0ea1e5778c7f47f747546bdd8170243a802ce
jpgonzalezo <juan.gonzalez.o@usach.cl>
2019-04-15 20:15:50 -0400

    UPDATE: Get profesores imagenes y diccionario

--- models/profesor.py
+++ models/profesor.py
@@ -10,9 +10,25 @@ class Profesor(gj.Document):
     apellido_materno = db.StringField(max_length=20)
     email = db.EmailField()
     telefono = db.StringField(max_length=12)
-    nombre_usuario = db.StringField(max_length=20)
     password = db.StringField(max_length=12)
     direccion = db.EmbeddedDocumentField(Direccion)
     rut = db.StringField(max_length=10)
     asignatura = db.ReferenceField(Asignatura)
+    imagen = db.StringField()
+    activo = db.BooleanField(default=True)
     meta = {'strict': False}
+
+    def to_dict(self):
+        return {
+            "id": str(self.id),
+            "nombres": self.nombres,
+            "apellido_paterno": self.apellido_paterno,
+            "apellido_materno": self.apellido_materno,
+            "email": self.email,
+            "telefono": self.telefono,
+            "password": self.password,
+            "direccion": self.direccion.to_dict(),
+            "rut": self.rut,
+            "asignatura": self.asignatura.to_dict(),
+            "imagen": self.imagen
+        }
--- resources/profesor.py
+++ resources/profesor.py
@@ -1,13 +1,17 @@
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify, request, send_file
 from models.profesor import Profesor
 from flask_restful import Api, Resource, url_for
 from libs.to_dict import mongo_to_dict
 import json
 from bson import json_util
+from PIL import Image
+import os
 
 def init_module(api):
     api.add_resource(ProfesorItem, '/profesores/<id>')
     api.add_resource(Profesores, '/profesores')
+    api.add_resource(ProfesorImagenItem, '/profesor_imagen/<id>')
+    api.add_resource(ProfesorImagenDefault, '/profesor_imagen_default/<id>')
 
 
 class ProfesorItem(Resource):
@@ -16,9 +20,36 @@ class ProfesorItem(Resource):
     
     def delete(self, id):
         profesor = Profesor.objects(id=id).first()
-        profesor.delete()
+        profesor.activo = False
+        profesor.save()
         return{'Response':'borrado'}
 
 class Profesores(Resource):
     def get(self):
-        return json.loads(Profesor.objects().all().to_json())
+        profesores = Profesor.objects().all()
+        response = []
+        for profesor in profesores:
+            if profesor.activo:
+                response.append(profesor.to_dict())
+        return response
+
+class ProfesorImagenItem(Resource):
+    def post(self,id):
+        profesor = Profesor.open(request.files['imagen'].stream).convert("RGB")
+        profesor.save(os.path.join("./uploads/profesores", str(id)+".jpg"))
+        profesor.thumbnail((800, 800))
+        profesor.save(os.path.join("./uploads/profesores", str(id)+'_thumbnail.jpg'))
+        profesor = Profesor.objects(id=id).first()
+        profesor.imagen = id
+        profesor.save()
+        return {'Response': 'exito'}
+    
+    def get(self,id):
+        return send_file('uploads/profesores/'+id+'_thumbnail.jpg')
+
+class ProfesorImagenDefault(Resource):
+    def get(self,id):
+        profesor = Profesor.objects(id=id).first()
+        profesor.imagen = "default"
+        profesor.save()
+        return { 'Response':'exito'}
